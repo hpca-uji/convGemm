d042906846fb25bec01902a211976e09f356218a
Andres Tomas <antodo@upv.es>
2021-11-12 11:34:12 +0100

    remove unused function

--- im2col_nchw.h
+++ im2col_nchw.h
@@ -8,8 +8,6 @@ void pack_CB_nchw_trans(char orderM, char transM, int mc, int nc, const float *M
 
 void pack_RB_nchw_trans(char orderM, char transM, int mc, int nc, const float *M, int ldM, float *Mc, int RR, const convol_dim *dim, int start_row, int start_col);
 
-void pack_transpose_nchw(int rows, int cols, const float *in, int ld, float *out, int b, int ho, int wo, int start_row, int start_col);
-
 void col2im_nchw(int m, int n, const float *cols, int ld, float *out, int batch, int channel, int height, int width, int oheight, int owidth, int kheight, int kwidth, int vpadding, int hpadding, int vstride, int hstride, int vdilation, int hdilation);
 
 void post_col2im_nchw(int n, int m, const float *cols, int ldc, float beta, float *out, int ldout, const convol_dim *d, const float *bias_vector, int start_col, int start_row, bool last);
